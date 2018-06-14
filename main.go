1720bb2668f4d8c6edfeac9f6bb1d2ab79576d19
Marina Nakagawa <mari10172001@yahoo.co.jp>
2018-06-14 16:41:22 +0900

    画面出力

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import(
 )
 
 func main() {
-    // os.Create新規ファイルの作成
-    file, err:=os.Create("text.txt")
-    if err!=nil{
-        panic(err)
-    }
-    //Write が受け取るのは文字列ではなくてバイト列だからバイト列に変換
-    //定義　func (f *File) Write(b []byte) (n int, err error) {
-    file.Write([]byte("os.File example\n"))
-    file.Close()
+    //前回の fmt.Println では、最終的に os.Stdout の Write メソッドを呼び出していました。
+    os.Stdout.Write([]byte("os.Stdout example\n"))
 }
