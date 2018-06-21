a02e80dd5a7b02dbf47318fa452b1b6c46abdbee
Marina Nakagawa <mari10172001@yahoo.co.jp>
2018-06-21 16:45:40 +0900

    標準出力

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import(
+	"fmt"
+	"io"
 	"os"
-	"encoding/json"
 )
 
 func main() {
-	encoder := json.NewEncoder(os.Stdout)
-	encoder.SetIndent(""," ")
-	encoder.Encode(map[string]string{
-		"example":"encoding/json",
-		"hello":"world",
-	})
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+			fmt.Printf("size=%d input='%s'\n", size, string(buffer))
+	}
 }
