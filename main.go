67a268f2acae3660bd5d3abde5727f4d732c0e10
Marina Nakagawa <mari10172001@yahoo.co.jp>
2018-06-28 17:10:55 +0900

    ルナの解析

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
 import(
-	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"os"
 )
 
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk,binary.BigEndian, &length)
+	buffer := make([]byte,4)
+	chunk.Read(buffer)
+	fmt.Printf("chunk'%v'(%d bytes)\n",string(buffer),length)
+}
+
+func readChunks(file *os.File) []io.Reader {
+	//チャンクを格納する配列
+	var chunks[]io.Reader
+	//最初に８バイトを飛ばす
+	file.Seek(8,0)
+	var offset int64 = 8
+
+	for {
+		var length int32
+		err := binary.Read(file, binary.BigEndian, &length)
+		if err == io.EOF {
+			break
+		}
+		chunks = append(chunks, io.NewSectionReader(file, offset, int64(length)+12))
+		//次のチャンクの先頭に移動
+		//現在の位置は長さを読み終わった箇所なので
+		//チャンク名(4バイト)+データ長+CRC(4バイト)先に移動
+		offset, _ = file.Seek(int64(length+8),1)
+	}
+	return chunks
+}
+
 func main() {
-	//32ビットのっビッグエンディアンのでーた(10000)
-	data := []byte{0x0,0x0,0x27,0x10}
-	var i int32
-	//エンディアンの変換
-	binary.Read(bytes.NewReader(data),binary.BigEndian,&i)
-	fmt.Printf("data: %d",i)
+	file, err := os.Open("Lenna.png")
+	if err != nil {
+		panic(err)
+	}
+	chunks := readChunks(file)
+	for _,chunk := range chunks {
+		dumpChunk(chunk)
+	}
 }
